fcc90d871bea63e6552cd999c612e8aca7071ed7
Monchusay <armydolgov@mail.ru>
2022-05-30 09:40:33 +0300

    0.2

--- src/Components/FirstPage/FirstPage.js
+++ src/Components/FirstPage/FirstPage.js
@@ -6,6 +6,7 @@ import HH from "../../Pictures/HH_logo.jpg";
 import Telegram from "../../Pictures/Telegram_logo.jpg";
 import Mail from "../../Pictures/Mail_logo.png";
 import Phone from "../../Pictures/Phone_logo.jpg";
+import Git from "../../Pictures/Git_logo.png";
 import { FormattedMessage } from "react-intl";
 import { LOCALES } from "../../i18n/locales";
 import { Link } from "react-scroll";
@@ -21,24 +22,40 @@ const FirstPage = ({ currentLocale, handleChange }) => {
       <div className={style.ContLangSelector}>
         <div className={style.Contacts}>
           <span className={style.ContactsElement}>
-            <img src={VK} />
-            <span className={style.ContactsElementText}></span>
+            <a href="https://vk.com/kudalov" target="_blank">
+              <img src={VK} />
+            </a>
           </span>
           <span className={style.ContactsElement}>
-            <img src={Telegram} />
-            <span className={style.ContactsElementText}></span>
+            <a href="https://t.me/omegapepeg" target="_blank">
+              <img src={Telegram} />
+            </a>
           </span>
           <span className={style.ContactsElement}>
-            <img src={Phone} />
-            <span className={style.ContactsElementText}></span>
+            <a href="tel:+79047897143">
+              <img src={Phone} />
+            </a>
           </span>
           <span className={style.ContactsElement}>
-            <img src={Mail} />
-            <span className={style.ContactsElementText}></span>
+            <a href="mailto:monchusay1@mail.ru">
+              <img src={Mail} />
+            </a>
           </span>
           <span className={style.ContactsElement}>
-            <img src={HH} />
-            <span className={style.ContactsElementText}></span>
+            <a
+              href="https://spb.hh.ru/resume/24bc33fbff09d719ae0039ed1f4f57376e3057"
+              target="_blank"
+            >
+              <img src={HH} />
+            </a>
+          </span>
+          <span className={style.ContactsGitElement}>
+            <a
+              href="https://github.com/Monchusay"
+              target="_blank"
+            >
+              <img src={Git} />
+            </a>
           </span>
         </div>
         <div className={style.LangSelector}>
@@ -56,7 +73,7 @@ const FirstPage = ({ currentLocale, handleChange }) => {
       <span className={style.FirstPageName}>
         <FormattedMessage id="mainInfo_name" />
       </span>
-      <span className={style.FirstPageProf}>Junior Frontend Developer</span>
+      <span className={style.FirstPageProf}>Frontend Developer</span>
       <span className={style.FirstPageSite}>
         <FormattedMessage id="mainInfo_content" />
       </span>
