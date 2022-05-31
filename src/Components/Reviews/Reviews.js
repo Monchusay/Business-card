f7115849776b8e1d60486cd11c4a11e3e9768a14
Monchusay <armydolgov@mail.ru>
2022-05-31 13:05:12 +0300

    0.2

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,46 +1,46 @@
 import React from "react";
 import style from "./Reviews.module.css";
-import {FormattedMessage} from "react-intl";
-import {FaChevronDown} from "react-icons/fa";
+import { FormattedMessage } from "react-intl";
+import { FaChevronDown } from "react-icons/fa";
 
 const Reviews = () => {
   return (
     <div className={style.Reviews}>
       <div className={style.ReviewsBox}>
-        <div className={style.Review}>
-            <div className={style.PicAndName}>
-                <div className={style.DimaPic}/>
-                <div className={style.NameBox}>
-                    <span className={style.Name}>
-                        <FormattedMessage id="reviews_DimaName" />
-                    </span>
-                    <span className={style.About}>
-                        <FormattedMessage id="reviews_DimaAbout" />
-                    </span>
-                </div>
+        <div className={style.DReview}>
+          <div className={style.PicAndName}>
+            <a href="https://vk.com/dman25" target="_blank">
+              <div className={style.DimaPic} />
+            </a>
+            <div className={style.NameBox}>
+              <span className={style.Name}>
+                <FormattedMessage id="reviews_DimaName" />
+              </span>
+              <span className={style.About}>
+                <FormattedMessage id="reviews_DimaAbout" />
+              </span>
             </div>
-            <div className={style.Content}>
-
-            </div>
+          </div>
+          <div className={style.Content}></div>
         </div>
-        <div className={style.Review}>
-            <div className={style.PicAndName}>
-                <div className={style.PolyaPic}/>
-                <div className={style.NameBox}>
-                    <span className={style.Name}>
-                        <FormattedMessage id="reviews_PolyaName" />
-                    </span>
-                    <span className={style.About}>
-                        <FormattedMessage id="reviews_PolyaAbout" />
-                    </span>
-                </div>
+        <div className={style.PReview}>
+          <div className={style.PicAndName}>
+            <a href="https://vk.com/frolova_poli" target="_blank">
+              <div className={style.PolyaPic} />
+            </a>
+            <div className={style.NameBox}>
+              <span className={style.Name}>
+                <FormattedMessage id="reviews_PolyaName" />
+              </span>
+              <span className={style.About}>
+                <FormattedMessage id="reviews_PolyaAbout" />
+              </span>
             </div>
-            <div className={style.Content}>
-
-            </div>
+          </div>
+          <div className={style.Content}></div>
         </div>
       </div>
-        <FaChevronDown className={style.Icon} />
+      <FaChevronDown className={style.Icon} />
     </div>
   );
 };
