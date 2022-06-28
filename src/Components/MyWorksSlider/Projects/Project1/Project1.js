dd1b9fdcac300454a1c4d3d4d73c39394b035014
Monchusay <armydolgov@mail.ru>
2022-06-28 18:48:18 +0300

    Final ver

--- src/Components/MyWorksSlider/Projects/Project1/Project1.js
+++ src/Components/MyWorksSlider/Projects/Project1/Project1.js
@@ -13,7 +13,7 @@ const Project1 = () => {
             <div className={style.ProjectPic}>
                 <div className={style.ProjectPicImg}/>
                 <a
-                    href="https://monchusay.github.io/ds-test-task-complited/"
+                    href="https://monchusay.github.io/ds-test-task-completed/"
                     target="_blank"
                     className={style.LinkToProj}
                 >
